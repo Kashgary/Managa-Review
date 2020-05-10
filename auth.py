2102c88d3a45f91dfb22e0ab70ed10898436149e
Kashgary <ab70x9@gmail.com>
2020-05-11 01:27:29 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'kashgary1.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'mangareview'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 '''
